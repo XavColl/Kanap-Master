854b08492a19907d2bc9b86009bd339620cb6fed
XavColl <Xavier.coll@hotmail.com>
2022-12-22 15:02:32 +0100

    function change

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,133 +1,162 @@
-const cartItems = document.getElementById('cart__items')
-let listDifferentProducts = []
+import { getFromLocalStorage } from "./localstorage.js";
+
+const cartItems = document.getElementById('cart__items');
+
+let productsFromCart = getFromLocalStorage();
+let productsFromApi = [];
 
 
-//get the local storage and transform it into an array of objects
-const getCart = () => {
-    console.log(localStorage)
-    const out = []
-    const net = localStorage.getItem('cart')
-    const listItems = net.split('|')
-    listItems.forEach((item) => {
-        const json = JSON.parse(item)
-        out.push(json)
-    })
-    return out
-}
 
+(async function init() {
+    await getProductsFromApi();
+    displayCart(productsFromCart);
+    
+})();
 
-//cleaning cart in local storage and returning the clean array
-const filterCart = (list) => {
-    const out = []
-    list.forEach((item) => {
-        let isAlreadyIn = false
-        out.forEach((cleanItem) => {
-            if(cleanItem.name === item.name && cleanItem.color === item.color){
-                cleanItem.quantity += parseInt(item.quantity)
-                isAlreadyIn = true
+async function getProductsFromApi() {
+    const ids = productsFromCart.map(product => product.id);
+    ids.forEach(async (id) => {
+        await fetch(`http://localhost:3000/api/products/${id}`).then((res) => {
+            if (res.ok) {
+                return res.json();
             }
         })
-        if(!isAlreadyIn){
-            out.push(item)
-        }
+        .then((value) => {
+            productsFromApi.push(value);
+            
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+        
     })
-    return out
 }
 
-//display cart
+function displayPrice(id){
+    const priceProduct = productsFromApi.find((product) => product._id === id);
+    const price = priceProduct.price;
+    return price;
+}
+
+function displayQuantity(){
+    let qt = 0;
+    productsFromCart.forEach( product => {
+        qt += parseInt(product.quantity);
+    })
+    document.getElementById('totalQuantity').textContent = qt;
+}
+
+function changeQuantity(e,color,id) {
+    console.log(productsFromCart)
+    const qt = e.target.value;
+    console.log(qt);
+    productsFromCart.find(product => product.color === color && product.id === id).quantity = qt;
+    localStorage.setItem('cart', JSON.stringify(productsFromCart))
+    displayTotal();
+    displayQuantity()
+}
+
+
+
+function displayTotal(){
+    let total = 0;
+    productsFromCart.forEach(product => {
+        const qt = product.quantity;
+        const brut = productsFromApi.find((el) => el._id === product.id);
+        const pr = brut.price;
+        total += qt * pr;
+    })
+    let str = total.toString();
+    if(str.length>3){
+        const arr = str.split('');
+        let pre = '';
+        let post = '';
+        while(arr.length - pre.length > 3 ){
+            const i = pre.length;
+            pre += '' + (arr[i]);
+        }
+        post = '' + arr[pre.length] + arr[pre.length+1] + arr[pre.length+2];
+        str = pre + ' ' + post;
+    }
+    const totalPrice = document.getElementById('totalPrice');
+    totalPrice.textContent = str;
+}
+
+// Display cart
+
 const displayCart = (list) => {
+    displayQuantity()
     list.forEach((item) => {
         fetch('http://localhost:3000/api/products/' + item.id)
         .then((res) => {
             if (res.ok) {
-                return res.json()
+                return res.json();
                 }
         })
         .then((value) => {
-           const newArticle = document.createElement('article')
-           const newCartItemImg = document.createElement('div')
-           const newImg = document.createElement('img')
-           const newCartItemContent = document.createElement('div')
-           const newCartItemContentDescription = document.createElement('div')
-           const newName = document.createElement('h2')
-           const newColor = document.createElement('p')
-           const newPrice = document.createElement('p')
-           const newCartItemContentSettings = document.createElement('div')
-           const newCartItemContentSettingsQuantity = document.createElement('div')
-           const newQuantity = document.createElement('p')
-           const newInput = document.createElement('input')
-           const newCartItemContentSettingsDelete = document.createElement('div')
-           const newDeleteItem = document.createElement('p')
+           const newArticle = document.createElement('article');
+           const newCartItemImg = document.createElement('div');
+           const newImg = document.createElement('img');
+           const newCartItemContent = document.createElement('div');
+           const newCartItemContentDescription = document.createElement('div');
+           const newName = document.createElement('h2');
+           const newColor = document.createElement('p');
+           const newPrice = document.createElement('p');
+           const newCartItemContentSettings = document.createElement('div');
+           const newCartItemContentSettingsQuantity = document.createElement('div');
+           const newQuantity = document.createElement('p');
+           const newInput = document.createElement('input');
+           const newCartItemContentSettingsDelete = document.createElement('div');
+           const newDeleteItem = document.createElement('p');
+           
 
-           newArticle.classList.add('cart__item')
-           newCartItemImg.classList.add('cart__item__img')
-           newCartItemContent.classList.add('cart__item__content')
-           newCartItemContentDescription.classList.add('cart__item__content__description')
-           newCartItemContentSettings.classList.add('cart__item__content__settings')
-           newInput.classList.add('itemQuantity')
-           newCartItemContentSettingsDelete.classList.add('cart__item__content__settings__delete')
-           newDeleteItem.classList.add('deleteItem')
+           newArticle.classList.add('cart__item');
+           newCartItemImg.classList.add('cart__item__img');
+           newCartItemContent.classList.add('cart__item__content');
+           newCartItemContentDescription.classList.add('cart__item__content__description');
+           newCartItemContentSettings.classList.add('cart__item__content__settings');
+           newInput.classList.add('itemQuantity');
+           newCartItemContentSettingsDelete.classList.add('cart__item__content__settings__delete');
+           newDeleteItem.classList.add('deleteItem');
 
-            newArticle.setAttribute('data-id', value.id)
-            newArticle.setAttribute('data-color', item.color)
-            newImg.setAttribute('src', value.imageUrl)
-            newImg.setAttribute('alt', value.altTxt)
-            newInput.setAttribute('type', 'number')
-            newInput.setAttribute('name', 'itemQuantity')
-            newInput.setAttribute('min', '1')
-            newInput.setAttribute('max', '100')
-            newInput.setAttribute('value', item.quantity)
+            newArticle.setAttribute('data-id', value._id);
+            newArticle.setAttribute('data-color', item.color);
+            newImg.setAttribute('src', value.imageUrl);
+            newImg.setAttribute('alt', value.altTxt);
+            newInput.setAttribute('type', 'number');
+            newInput.setAttribute('name', 'itemQuantity');
+            newInput.setAttribute('min', '1');
+            newInput.setAttribute('max', '100');
+            newInput.setAttribute('value', item.quantity);
 
-            newName.textContent = item.name
-            newColor.textContent = item.color
-            newPrice.textContent = item.price + ',00 €'
-            newQuantity.textContent = 'Qté : '
-            newDeleteItem.textContent = 'Supprimer'
+            newName.textContent = item.name;
+            newColor.textContent = item.color;
+            newPrice.textContent = displayPrice(value._id) + ',00 €';
+            newQuantity.textContent = 'Qté : ';
+            newDeleteItem.textContent = 'Supprimer';
 
-            newCartItemContentSettingsDelete.append(newDeleteItem)
-            newCartItemContentSettingsQuantity.append(newQuantity)
-            newCartItemContentSettingsQuantity.append(newInput)
-            newCartItemContentSettings.append(newCartItemContentSettingsQuantity)
-            newCartItemContentSettings.append(newCartItemContentSettingsDelete)
-            newCartItemContentDescription.append(newName)
-            newCartItemContentDescription.append(newColor)
-            newCartItemContentDescription.append(newPrice)
-            newCartItemContent.append(newCartItemContentDescription)
-            newCartItemContent.append(newCartItemContentSettings)
-            newCartItemImg.append(newImg)
-            newArticle.append(newCartItemImg)
-            newArticle.append(newCartItemContent)
-            cartItems.append(newArticle)
-            setTotalQuantity()
-            setTotalPrice()
+            newCartItemContentSettingsDelete.append(newDeleteItem);
+            newCartItemContentSettingsQuantity.append(newQuantity);
+            newCartItemContentSettingsQuantity.append(newInput);
+            newCartItemContentSettings.append(newCartItemContentSettingsQuantity);
+            newCartItemContentSettings.append(newCartItemContentSettingsDelete);
+            newCartItemContentDescription.append(newName);
+            newCartItemContentDescription.append(newColor);
+            newCartItemContentDescription.append(newPrice);
+            newCartItemContent.append(newCartItemContentDescription);
+            newCartItemContent.append(newCartItemContentSettings);
+            newCartItemImg.append(newImg);
+            newArticle.append(newCartItemImg);
+            newArticle.append(newCartItemContent);
+            cartItems.append(newArticle);
+            displayTotal();
+
+            newInput.addEventListener('change', (e) => changeQuantity(e,item.color,value._id))
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
         })
     })
 }
 
 
-//display total quantity
-const setTotalQuantity = () => {
-    let total = 0
-    const brut = localStorage.getItem('cart')
-    brut.split('|').forEach((item) => {
-        const itemJson = JSON.parse(item)
-        total += itemJson.quantity
-    })
-    document.getElementById('totalQuantity').textContent = total
-}
-
-//display total price
-const setTotalPrice = () => {
-    let total = 0
-    const brut = localStorage.getItem('cart')
-    brut.split('|').forEach((item) => {
-        const itemJson = JSON.parse(item)
-        total += itemJson.price
-    })
-    document.getElementById('totalPrice').textContent = total
-}
-
-displayCart(filterCart(getCart()))
--- front/js/product.js
+++ front/js/product.js
@@ -1,54 +1,69 @@
-const id = location.search.split('=')[1]
+import { getFromLocalStorage } from "./localstorage.js";
 
+const href = window.location.href
+const url = new URL(href);
+const id = url.searchParams.get("id");
+let productsList = [];
 
-//displaying one product according to the id in the url
-const displayOneProduct = async () => {
-    fetch('http://localhost:3000/api/products/' + id).then((res) => {
+const quantity = document.getElementById('quantity');
+const colorsSelect = document.getElementById('color-select');
+
+(function init() {
+    displayOneProduct();
+    productsList = getFromLocalStorage();
+
+    const buttonSubmit = document.getElementById('addToCart');
+    buttonSubmit.addEventListener('click', addToCart);
+})();
+
+// Displaying one product according to the id in the url
+function displayOneProduct() {
+    fetch(`http://localhost:3000/api/products/${id}`).then((res) => {
         if (res.ok) {
-            return res.json()
-            }
+            return res.json();
+        }
     })
     .then((value) => {
         value.colors.forEach((color) => {
-            const newOption = document.createElement('option')
-            newOption.setAttribute('value', color)
-            newOption.textContent = color
-            document.getElementById('colors').append(newOption)
-        })
+            const newOption = document.createElement('option');
+            newOption.setAttribute('value', color);
+            newOption.textContent = color;
+            colorsSelect.append(newOption);
+        });
 
-        const newImg = document.createElement('img')
+        const newImg = document.createElement('img');
 
-        newImg.setAttribute('src', value.imageUrl)
-        newImg.setAttribute('alt', value.altTxt)
+        newImg.setAttribute('src', value.imageUrl);
+        newImg.setAttribute('alt', value.altTxt);
 
-        document.querySelector('.item__img').append(newImg)
-        document.getElementById('title').textContent = value.name
-        document.getElementById('price').textContent = value.price
-        document.getElementById('description').textContent = value.description
-        
-    
+        document.querySelector('.item__img').append(newImg);
+        document.getElementById('title').textContent = value.name;
+        document.getElementById('price').textContent = value.price;
+        document.getElementById('description').textContent = value.description;
     })
-    
     .catch((err) => {
-        console.log(err)
+        console.log(err);
     })
 }
 
+// Function triggered by the button, which adds items in local storage
 
-//Function triggered by the button, which adds items in local storage
-const addToCart = () => {
+function addToCart() {
+    if(!colorsSelect.value) return // disabling if the color is not set
     const obj = {
-        name : document.getElementById('title').textContent,
-        price : parseInt(document.getElementById('price').textContent),
-        color : document.getElementById('colors').value,
-        quantity : parseInt(document.getElementById('quantity').value),
+        color : colorsSelect.value,
+        quantity : parseInt(quantity.value),
         id
+    };
+    let isAlreadyIn = false;
+    productsList.forEach(product => {
+        if(product.id === obj.id && product.color === obj.color){
+            isAlreadyIn = true;
+            product.quantity += obj.quantity;
+        }
+    })
+    if(isAlreadyIn === false){
+        productsList.push(obj);
     }
-    if(!localStorage.getItem('cart')){
-        localStorage.setItem('cart', JSON.stringify(obj)) 
-    }else localStorage.setItem('cart', localStorage.getItem('cart') + '|' + JSON.stringify(obj))
-    
+    localStorage.setItem('cart', JSON.stringify(productsList));
 }
-
-
-displayOneProduct()
